bdb81d85e9524578d12af9d1ac724aec9f6679df
Christopher Marrai <117444333+Chicco123uk@users.noreply.github.com>
2022-11-04 12:33:13 +0000

    changes 3

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import math
 import random
 
-print("Hello world")
-
 num = 0
 
 def generate_random_number():
   math.random()
   print(random.randint(1,9))
+
+
+def difference_from_answer(guess,answer):
+  answer = generate_random_number()
+  guess = int(input("Input a number 1 to 9"))
+  if guess == answer:
+    print("Correct")
+  if guess < answer:
+    print("Too Low")
+  else:
+    print("Too High")
