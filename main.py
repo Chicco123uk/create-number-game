f51300b801bc810d36f2ea3dc019f8357e906204
Christopher Marrai <117444333+Chicco123uk@users.noreply.github.com>
2022-11-04 12:46:38 +0000

    ignore my final commit but accept these corrections with yet another pinch of salt :-(

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import random
 num = random.randint(1, 9)
 guess = int(input("Enter an integer from 1 to 9: "))
 while num != "Guess":
-    print
-    if guess < num:
-      print ("Guess is low")
-      guess = int(input("Enter an integer from 1 to 9: "))
+  print
+  if guess < num:
+    print ("Guess is low")
+    guess = int(input("Enter an integer from 1 to 9: "))
+    
+  elif guess > num:
+    print ("Guess is high")
+    guess = int(input("Enter an integer from 1 to 9: "))
       
-    elif guess > num:
-      print ("Guess is high")
-      guess = int(input("Enter an integer from 1 to 9: "))
-      
-    else:
-      print ("you guessed it!")
-      break
-    print
+  else:
+    print ("You guessed it!")
+    break
+  print
