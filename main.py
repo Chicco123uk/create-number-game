b545058a084e39d0b9d6e5bb0d4f4e7b7529c507
Christopher Marrai <117444333+Chicco123uk@users.noreply.github.com>
2022-11-04 12:52:16 +0000

    added to function

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import random
-num = random.randint(1, 9)
-guess = int(input("Enter an integer from 1 to 9: "))
-while num != "Guess":
-  print
-  if guess < num:
-    print ("Guess is low")
-    guess = int(input("Enter an integer from 1 to 9: "))
-    
-  elif guess > num:
-    print ("Guess is high")
-    guess = int(input("Enter an integer from 1 to 9: "))
+
+
+def randomnumbergame():
+  num = random.randint(1, 9)
+  guess = int(input("Enter an integer from 1 to 9: "))
+  while num != "Guess":
+    print
+    if guess < num:
+      print ("Guess is low")
+      guess = int(input("Enter an integer from 1 to 9: "))
       
-  else:
-    print ("You guessed it!")
-    break
-  print
+    elif guess > num:
+      print ("Guess is high")
+      guess = int(input("Enter an integer from 1 to 9: "))
+        
+    else:
+      print ("You guessed it!")
+      break
+    print
+
+randomnumbergame()
